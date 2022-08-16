15630215fab80501ec908b828d853341e85394b3
HerrIves <spechtator@gmail.com>
2022-08-16 17:06:14 +0200

    init commit lesson 2

---
+++ src/main/java/de/ivev/spring/TestBean.java
@@ -0,0 +1,17 @@
+package de.ivev.spring;
+
+public class TestBean{
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
