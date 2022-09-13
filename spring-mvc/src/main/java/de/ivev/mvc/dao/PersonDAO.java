165aacfc05ea58b17d28494849ffba4ca968f4dd
HerrIves <spechtator@gmail.com>
2022-09-13 16:09:05 +0200

    mvc l8 completed

---
+++ spring-mvc/src/main/java/de/ivev/mvc/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package de.ivev.mvc.dao;
+
+import de.ivev.mvc.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int peopleCount;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++peopleCount, "Tom"));
+        people.add(new Person(++peopleCount, "Bob"));
+        people.add(new Person(++peopleCount, "Mike"));
+        people.add(new Person(++peopleCount, "Katy"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream()
+                .filter(person -> person.getId() == id)
+                .findAny().orElse(new Person(0, "null"));
+
+    }
+}
