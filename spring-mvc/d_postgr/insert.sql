74926b22e4e7fc39d00e6173245943e68e866ba7
HerrIves <spechtator@gmail.com>
2022-09-16 12:45:13 +0200

    mvc l 12 completed

---
+++ spring-mvc/d_postgr/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO person VALUES (1, 'Tom', 18, 'tom@mail.com');
+INSERT INTO person VALUES (2, 'Bob', 28, 'bob@mail.com');
+INSERT INTO person VALUES (3, 'John', 38, 'john@mail.com');
+INSERT INTO person VALUES (1, 'Kate', 48, 'kate@mail.com');
--- spring-mvc/src/main/java/de/ivev/mvc/dao/PersonDAO.java
+++ spring-mvc/src/main/java/de/ivev/mvc/dao/PersonDAO.java
@@ -3,48 +3,90 @@ package de.ivev.mvc.dao;
 import de.ivev.mvc.models.Person;
 import org.springframework.stereotype.Component;
 
+import java.net.ConnectException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private static int peopleCount;
-    private List<Person> people;
 
-    {
-        people = new ArrayList<>();
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "root";
 
-        people.add(new Person(++peopleCount, "Tom", 20, "tom@mail.com"));
-        people.add(new Person(++peopleCount, "Bob", 31, "bob@mail.com"));
-        people.add(new Person(++peopleCount, "Mike", 41, "mike@mail.com"));
-        people.add(new Person(++peopleCount, "Katy", 34, "katy@mail.com"));
+    private static Connection connection;
+
+    static{
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<Person> index(){
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()){
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return people;
     }
 
     public Person show(int id){
-        return people.stream()
-                .filter(person -> person.getId() == id)
-                .findAny().orElse(new Person(0, "NULL", 0, "NULL"));
+//        return people.stream()
+//                .filter(person -> person.getId() == id)
+//                .findAny().orElse(new Person(0, "NULL", 0, "NULL"));
+        return null;
     }
 
     public void save(Person person) {
-        person.setId(++peopleCount);
-        people.add(person);
+//        person.setId(++peopleCount);
+//        people.add(person);
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName()
+                    + "'," + person.getAge() + ",'" + person.getEmail() + "')";
+            statement.executeUpdate(SQL);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void update(int id, Person person) {
-        Person person2Update = show(id);
-        person2Update.setName(person.getName());
-        person2Update.setAge(person.getAge());
-        person2Update.setEmail(person.getEmail());
-
+//        Person person2Update = show(id);
+//        person2Update.setName(person.getName());
+//        person2Update.setAge(person.getAge());
+//        person2Update.setEmail(person.getEmail());
     }
 
     public void delete(int id) {
-        Person person4Delete = show(id);
-        people.remove(person4Delete);
+//        Person person4Delete = show(id);
+//        people.remove(person4Delete);
     }
 }
