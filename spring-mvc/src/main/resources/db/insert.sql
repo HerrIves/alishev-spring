48d999488884aea8b44c6f1dc80e912dbe4a6275
HerrIves <spechtator@gmail.com>
2022-09-26 15:57:02 +0200

    sql done

--- spring-mvc/src/main/resources/db/insert.sql
+++ spring-mvc/src/main/resources/db/insert.sql
@@ -1,4 +1,23 @@
-INSERT INTO person VALUES (1, 'Tom', 18, 'tom@mail.com');
-INSERT INTO person VALUES (2, 'Bob', 28, 'bob@mail.com');
-INSERT INTO person VALUES (3, 'John', 38, 'john@mail.com');
-INSERT INTO person VALUES (1, 'Kate', 48, 'kate@mail.com');
+INSERT INTO person(name, age, email)VALUES ('Tom', 18, 'tom@mail.com');
+INSERT INTO person(name, age, email)VALUES ('Bob', 28, 'bob@mail.com');
+INSERT INTO person(name, age, email)VALUES ('John', 38, 'john@mail.com');
+INSERT INTO person(name, age, email)VALUES ('Kate', 48, 'kate@mail.com');
+
+INSERT INTO director(name, age)VALUES ('Qentin tarantino', 57);
+INSERT INTO director(name, age)VALUES ('martin Scorsese', 78);
+INSERT INTO director(name, age)VALUES ('gue ritchie', 52);
+INSERT INTO director(name, age)VALUES ('woody allen', 85);
+INSERT INTO director(name, age)VALUES ('david lynch', 74);
+INSERT INTO director(name, age)VALUES ('christopher noland', 50);
+
+insert into movie(director_id, name , year_of_production ) VALUES ( 1 , ' Reservoir Dogs ' , 1992);
+insert into movie(director_id, name , year_of_production ) VALUES ( 1 , ' Pulp Fiction ' , 1994 );
+insert into movie(director_id, name , year_of_production ) VALUES ( 1 , ' The Hateful Eight ' , 2015 ) ;
+insert into movie(director_id, name , year_of_production ) VALUES ( 1 , ' Once Upon a Time in Hollywood ' , 2019 ) ;
+insert into movie(director_id, name , year_of_production ) VALUES ( 2 , ' Taxi Driver ' , 1976 ) ;
+insert into movie(director_id, name , year_of_production ) VALUES ( 2 , ' Goodfellas ' , 1990 ) ;
+insert into movie(director_id, name , year_of_production ) VALUES ( 2 , ' The Wolf of Wall Street ' , 2013 ) ;
+insert into movie(director_id, name , year_of_production ) VALUES ( 3 , ' Lock , Stock and Two Smoking Barrels ' , 1998);
+insert into movie(director_id, name , year_of_production ) VALUES ( 3 , ' Snatch ' , 2000 ) ;
+insert into movie(director_id, name , year_of_production ) VALUES ( 4 , ' Midnight in Paris ' , 2011 ) ;
+insert into movie(director_id, name , year_of_production ) VALUES ( 6 , ' Inception ' , 2010 ) ;
