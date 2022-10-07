a7b3e4b9928bf9c473db1cb4837cbd5d2c74a561
HerrIves <spechtator@gmail.com>
2022-10-07 16:37:21 +0200

    hibernate 06 getting person & items from db

--- hibernate/src/main/java/org/example/App.java
+++ hibernate/src/main/java/org/example/App.java
@@ -1,30 +1,39 @@
 package org.example;
 
 
+import org.example.model.Item;
+import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.List;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) {
-        Person person1 = new Person("mike", 20);
-        Person person3 = new Person("fike", 40);
+public class App {
+    public static void main(String[] args) {
 
-        SessionFactory sessionFactory = new Configuration().addAnnotatedClass(Person.class).buildSessionFactory();
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
 
-        session.save(person1);
-        session.save(person3);
+        try {
+            session.beginTransaction();
 
-        session.getTransaction().commit();
-        sessionFactory.close();
+            Person person = session.get(Person.class, 4);
+            System.out.println(person);
+            List<Item> items = person.getItems();
+            System.out.println(items);
+            session.getTransaction().commit();
+        }finally {
+            sessionFactory.close();
+        }
 
+
+        }
     }
-}
