a67f752db356de9d7d17ae56992583f7de0509ba
HerrIves <spechtator@gmail.com>
2023-01-13 12:03:40 +0100

    t11.11 login ready. authorisation next

--- spring-security/src/main/java/de/ivev/FirstSecurityApp/config/SecurityConfig.java
+++ spring-security/src/main/java/de/ivev/FirstSecurityApp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import de.ivev.FirstSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -13,19 +14,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    private final AuthProviderImpl authProvider;
-//    @Autowired
-//    public SecurityConfig(AuthProviderImpl authProvider) {
-//        this.authProvider = authProvider;
-//    }
-
-    @Autowired
     private final PersonDetailsService personDetailsService;
 
+    @Autowired
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/auth/login")
+                .loginProcessingUrl("process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
