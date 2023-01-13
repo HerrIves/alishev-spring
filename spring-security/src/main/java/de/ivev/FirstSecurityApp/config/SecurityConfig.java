2743cdbb3eeb5cf8fdef1b943d73b373e2484ac2
HerrIves <spechtator@gmail.com>
2023-01-13 10:53:12 +0100

    l80 default securuty authorisation done

--- spring-security/src/main/java/de/ivev/FirstSecurityApp/config/SecurityConfig.java
+++ spring-security/src/main/java/de/ivev/FirstSecurityApp/config/SecurityConfig.java
@@ -1,23 +1,37 @@
 package de.ivev.FirstSecurityApp.config;
 
-import de.ivev.FirstSecurityApp.security.AuthProviderImpl;
+import de.ivev.FirstSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+//    private final AuthProviderImpl authProvider;
+//    @Autowired
+//    public SecurityConfig(AuthProviderImpl authProvider) {
+//        this.authProvider = authProvider;
+//    }
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    private final PersonDetailsService personDetailsService;
+
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
