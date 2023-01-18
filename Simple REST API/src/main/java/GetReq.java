5478c19d39911f3cf1cb1c2f3cad755f436fd4a9
HerrIves <spechtator@gmail.com>
2023-01-18 15:28:47 +0100

    first rest requests

---
+++ Simple REST API/src/main/java/GetReq.java
@@ -0,0 +1,27 @@
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetReq {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String getUrl = "https://reqres.in/api/users/2";
+
+        String getResp = restTemplate.getForObject(getUrl, String.class);
+        System.out.println();
+        System.out.println(getResp);
+
+        String postURL = "https://reqres.in/api/users";
+        Map<String, String> json2Send = new HashMap<>();
+        json2Send.put("name", "Evgeny");
+        json2Send.put("job", "Java Dev");
+        HttpEntity<Map<String,String>>postRequest = new HttpEntity<>(json2Send);
+
+        String PostResp = restTemplate.postForObject(postURL, postRequest, String.class);
+        System.out.println(postRequest);
+        System.out.println(PostResp);
+    }
+}
