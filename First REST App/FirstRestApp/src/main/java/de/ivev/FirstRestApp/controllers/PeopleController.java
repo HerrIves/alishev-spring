2cbd34fb13e8c57feb1e8c01a1f15757e7bc0650
HerrIves <spechtator@gmail.com>
2023-01-19 17:40:15 +0100

    peopleController ready

--- First REST App/FirstRestApp/src/main/java/de/ivev/FirstRestApp/controllers/PeopleController.java
+++ First REST App/FirstRestApp/src/main/java/de/ivev/FirstRestApp/controllers/PeopleController.java
@@ -1,7 +1,37 @@
 package de.ivev.FirstRestApp.controllers;
 
-import org.springframework.stereotype.Controller;
+import de.ivev.FirstRestApp.models.Person;
+import de.ivev.FirstRestApp.services.PeopleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/people")
 public class PeopleController {
+    private final PeopleService peopleService;
+
+    @Autowired
+    public PeopleController(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @GetMapping()
+    public List<Person> findAll(){
+        return peopleService.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable("id") int id){
+        Person one = peopleService.findOne(id);
+        if (one==null) {
+            one = new Person();
+            one.setId(id);
+            one.setName("no such person");
+        }
+        return one;
+    }
 }
