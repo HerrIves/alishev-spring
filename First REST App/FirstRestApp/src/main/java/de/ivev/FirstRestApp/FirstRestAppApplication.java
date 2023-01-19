9cb6203a8d4ce92168b5c8b2fef986782c159e84
HerrIves <spechtator@gmail.com>
2023-01-19 13:11:13 +0100

    first rest method added + git ignore to old folders added

---
+++ First REST App/FirstRestApp/src/main/java/de/ivev/FirstRestApp/FirstRestAppApplication.java
@@ -0,0 +1,13 @@
+package de.ivev.FirstRestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestAppApplication.class, args);
+	}
+
+}
